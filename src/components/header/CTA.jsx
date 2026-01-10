9c92e8fcadc7e2d61a50e6e4db3909610c3b17f3
Mayuri Yedlapalli <mayuriyedlapalli@Mayuris-MacBook-Pro.local>
2026-01-10 11:34:43 +0100

    resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Mayuri_Vaddempudi_CV from "../../assets/cv.pdf"
+import Mayuri_Vaddempudi_CV from "../../assets/Mayuri_Vaddempudi_CV.pdf"
 
 export default function CTA() {
     return (
