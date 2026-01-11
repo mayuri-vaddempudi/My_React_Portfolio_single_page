4d50c48fbf90cd229abf304cdc54f6405d86c0b6
Mayuri Yedlapalli <mayuriyedlapalli@Mayuris-MacBook-Pro.local>
2026-01-11 13:52:02 +0100

    heading

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,21 +4,21 @@ import ME from "../../assets/me.jpg";
 import CTA from "./CTA";
 import HeaderSocials from "./HeaderSocials";
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <header>
             <div className="container header_container">
                 <h5>Hello I'm</h5>
                 <h1>Mayuri Vaddempudi</h1>
-                <h5 className="text-light">Fullstack Developer</h5>
-                <CTA/>
-                <HeaderSocials/>
+                <h5 className="text-light">Froneend Developer</h5>
+                <CTA />
+                <HeaderSocials />
                 <div className="me">
-                    <img src={ME} alt="me"/>
+                    <img src={ME} alt="me" />
                 </div>
                 <a href="#contact" className="scroll_down">Scroll Down</a>
             </div>
         </header>
-        
+
     )
 }
